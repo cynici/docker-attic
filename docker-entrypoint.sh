43bcf3dfb11c9d40e54f0db6b01e992b8d534858
Cheewai Lai <cheewai.lai@gmail.com>
2018-12-04 08:05:55 +0200

    fix: Cater for root user to run attic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,23 @@
 #! /bin/sh
 set -u
 [ -n "${DEBUG:-}" ] && set -x
-RUNUSER_UID="${RUNUSER_UID:-1000}"
-RUNUSER_GID="${RUNUSER_GID:-1000}"
+
+RUNUSER="${RUNUSER:-root}"
+RUNUSER_SHELL="${RUNUSER_SHELL:-/bin/sh}"
+
+if [ "$RUNUSER" = "root" ] ; then
+    RUNUSER_HOME=/root
+    RUNUSER_UID=0
+    RUNUSER_GID=0
+else
+    RUNUSER_HOME="/home/$RUNUSER"
+    RUNUSER_UID="${RUNUSER_UID:-1000}"
+    RUNUSER_GID="${RUNUSER_GID:-1000}"
+fi
+
 # Refer to busybox 'adduser' manpage for details
-addgroup -g ${RUNUSER_GID} runuser
-adduser -s /bin/false -D -h /home/runuser -H -u ${RUNUSER_UID} -G runuser runuser
-mkdir -p /home/runuser/.attic /home/runuser/.cache/attic
-chown -R runuser:runuser /home/runuser/.attic /home/runuser/.cache/attic
-exec su-exec runuser attic "$@"
+[ $RUNUSER_UID -ne 0 ] && addgroup -g ${RUNUSER_GID} $RUNUSER
+[ $RUNUSER_UID -ne 0 ] && adduser -s $RUNUSER_SHELL -D -h $RUNUSER_HOME -H -u ${RUNUSER_UID} -G $RUNUSER $RUNUSER
+mkdir -p $RUNUSER_HOME/.attic $RUNUSER_HOME/.cache/attic
+chown -R $RUNUSER:$RUNUSER $RUNUSER_HOME/.attic $RUNUSER_HOME/.cache/attic
+exec su-exec $RUNUSER attic "$@"
